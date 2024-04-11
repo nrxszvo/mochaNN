dd8fe7960df0fef86f1ebfb1a5a44c8faa11d9f3
Michael Horgan <michaelghorgan@gmail.com>
2024-04-11 19:43:15 +0000

    add resample_points arg to dataset generation script

--- generate_dataset.py
+++ generate_dataset.py
@@ -2,7 +2,7 @@ import numpy as np
 import dysts.flows as flows
 
 
-def make_multi_ic(name, n_ic, seqlen, ic_perturb, fn=None):
+def make_multi_ic(name, n_ic, seqlen, ic_perturb, resample_points, fn=None):
     if fn is None:
         fn = f"{name}_{n_ic}x{seqlen}_ic-{ic_perturb}"
     model = getattr(flows, name)()
@@ -11,7 +11,10 @@ def make_multi_ic(name, n_ic, seqlen, ic_perturb, fn=None):
     perturb = 1 + ic_perturb - (2 * ic_perturb) * np.random.random((n_ic, ndim))
     perturb[0] = np.ones(ndim)  # first ic is default
     model.ic = model.ic[None, :] * perturb
-    tpts, sol = model.make_trajectory(n=seqlen, return_times=True)
+    print(f"upscale factor: {model_md['period'] / (resample_points * model.dt):.2f}")
+    tpts, sol = model.make_trajectory(
+        n=seqlen, return_times=True, pts_per_period=resample_points
+    )
 
     dataset = {
         "model": name,
@@ -47,6 +50,19 @@ if __name__ == "__main__":
         help="number of unique initial conditions to generate",
         type=int,
     )
+    parser.add_argument(
+        "--resample_points",
+        default=100,
+        type=int,
+        help="resample points per period",
+    )
     parser.add_argument("--fn", default=None, help="output filename")
     args = parser.parse_args()
-    make_multi_ic(args.model, args.num_ic, args.seqlen, args.ic_perturb, args.fn)
+    make_multi_ic(
+        args.model,
+        args.num_ic,
+        args.seqlen,
+        args.ic_perturb,
+        args.resample_points,
+        args.fn,
+    )
